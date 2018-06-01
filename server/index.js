f90ead2623a7b707ffa516e2c9bb27de5b374fc7
jackson-lenhart <jacksonlenhart@gmail.com>
2018-05-31 20:45:22 -0400

    Fix path

--- server/index.js
+++ server/index.js
@@ -32,5 +32,5 @@ app.use("/api/applicant", applicant);
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
