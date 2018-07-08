15f43792b83b82e5045fe016eda9d9142a9e4c5d
Zack Martin <zack@Zacks-Air.home>
2018-07-08 16:57:43 -0400

    made minor edits to final results

--- src/containers/Company/FinalResults/FinalResults.js
+++ src/containers/Company/FinalResults/FinalResults.js
@@ -1,87 +1,88 @@
-import React from 'react';
+import React from "react";
 // import Aux from '../../../hoc/Aux/Aux';
 // import { Link } from 'react-router-dom';
-import Spinner from '../../../components/UI/Spinner/Spinner';
-import TestResults from './TestResults/TestResults';
+import Spinner from "../../../components/UI/Spinner/Spinner";
+import TestResults from "./TestResults/TestResults";
 
 class FinalResults extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            isError: false,
-            data: {}
-        };
-        this.ApplicantId = this.props.match.params.ApplicantId;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      isError: false,
+      data: {},
+      applicants: []
+    };
+    this.ApplicantId = this.props.match.params.ApplicantId;
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    if (token === null) {
+      this.setState({
+        isLoading: false,
+        isError: true
+      });
+      return;
     }
 
-    // componentDidMount() {
-    //     const token = localStorage.getItem("token");
-    //     if (token === null) {
-    //         this.setState({
-    //             isLoading: false,
-    //             isError: true
-    //         });
-    //         return;
-    //     }
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
 
-    //     const options = {
-    //         headers: {
-    //             "Authorization": `Bearer ${token}`
-    //         }
-    //     };
+    fetch("http://localhost:4567/api/company/test-results/SkjxVtEZX", options)
+      .then(
+        res => (res.status === 403 ? Promise.reject("Auth denied") : res.json())
+      )
+      .then(data => {
+        console.log("DATA FROM FINALRESULTS", data);
+        this.setState({
+          data,
+          isLoading: false
+        });
+        console.log(this.state.data);
+      })
+      .catch(err => console.error(err));
+  }
 
-    //     fetch(`http://localhost:4567/api/company/test-results/${this.ApplicantId}`, options)
-    //     .then(res =>
-    //         res.status === 403 ?
-    //             Promise.reject("Auth denied") :
-    //             res.json()
-    //     ).then(data => {
-    //         console.log("DATA FROM FINALRESULTS", data);
-    //         this.setState({
-    //             data,
-    //             isLoading: false
-    //         });
-    //     }).catch(err => console.error(err));
+  // formattedSeconds = (sec) => {
+  //     return (Math.floor(sec / 60) +
+  //         ' minutes and ' +
+  //       ('0' + sec % 60).slice(-2) + ' seconds');
+  // }
+
+  render() {
+    // if (this.state.isLoading) {
+    //     return <Spinner />;
     // }
 
-
-    // formattedSeconds = (sec) => {
-    //     return (Math.floor(sec / 60) +
-    //         ' minutes and ' +
-    //       ('0' + sec % 60).slice(-2) + ' seconds');
+    // let answers = this.state.data.answers;
+    // let content = [];
+    // for (let k in answers) {
+    //     content.push(
+    //         <div key={k}>
+    //             <p>{this.state.data.test.questions.find(x =>
+    //                 x.id === k
+    //             ).body}:</p>
+    //             <p><em>{answers[k]}</em></p>
+    //         </div>
+    //     );
     // }
 
-    render() {
-        // if (this.state.isLoading) {
-        //     return <Spinner />;
-        // }
-
-        // let answers = this.state.data.answers;
-        // let content = [];
-        // for (let k in answers) {
-        //     content.push(
-        //         <div key={k}>
-        //             <p>{this.state.data.test.questions.find(x =>
-        //                 x.id === k
-        //             ).body}:</p>
-        //             <p><em>{answers[k]}</em></p>
-        //         </div>
-        //     );
-        // }
-
-        return (
-            <TestResults />
-            // <Aux>
-            //     <div style={{padding: '20px', textAlign: 'left'}}>
-            //         <Link to='/company' style={{textDecoration: 'none', color: 'white', padding: '10px', cursor: 'pointer', boxShadow: '2px 2px 1px 0px rgba(0,0,0,0.75)', backgroundColor: 'purple'}}>BACK</Link>
-            //     </div>
-            //     <h3 style={{color: 'purple'}}>Results for {this.state.data.firstName} {this.state.data.lastName}</h3>
-            //     <h4 style={{color: 'purple'}}>Total amount of time taken is <span style={{color: 'red', textDecoration: 'underline'}}>{this.formattedSeconds(this.state.data.secondsElapsed)}</span></h4>
-            //     {content}
-            // </Aux>
-        );
-    }
+    return (
+      <TestResults />
+      // <Aux>
+      //     <div style={{padding: '20px', textAlign: 'left'}}>
+      //         <Link to='/company' style={{textDecoration: 'none', color: 'white', padding: '10px', cursor: 'pointer', boxShadow: '2px 2px 1px 0px rgba(0,0,0,0.75)', backgroundColor: 'purple'}}>BACK</Link>
+      //     </div>
+      //     <h3 style={{color: 'purple'}}>Results for {this.state.data.firstName} {this.state.data.lastName}</h3>
+      //     <h4 style={{color: 'purple'}}>Total amount of time taken is <span style={{color: 'red', textDecoration: 'underline'}}>{this.formattedSeconds(this.state.data.secondsElapsed)}</span></h4>
+      //     {content}
+      // </Aux>
+    );
+  }
 }
 
 export default FinalResults;
